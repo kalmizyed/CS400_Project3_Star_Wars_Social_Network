8047329f0ec89fe525349479bb30204a1899ee82
Joseph Cai <joe734647@gmail.com>
2022-04-13 19:53:40 -0500

    Added file header

--- IGraphBuilder.java
+++ IGraphBuilder.java
@@ -1,3 +1,10 @@
+// --== CS400 Project Three File Header ==--
+// Name: Joseph Cai
+// CSL Username: josephc
+// Email: jbcai@wisc.edu
+// Lecture #: 004 @4:00pm
+// Notes to Grader:
+
 public interface IGraphBuilder {
     /**
      * Reads the JSON input file and creates a graph
