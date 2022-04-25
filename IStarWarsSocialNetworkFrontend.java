be62724988363557e8aaa5c3db5568edb3414297
Jack Blake <jhblake2@wisc.edu>
2022-04-25 13:07:16 -0500

    changed name of parameter in frontend interface's displayMessage method from pathLength to degrees

--- IStarWarsSocialNetworkFrontend.java
+++ IStarWarsSocialNetworkFrontend.java
@@ -26,10 +26,10 @@ public interface IStarWarsSocialNetworkFrontend {
 	public void loadWordsWithPrefixInDropdown(ListView<String> dropdown, String prefix);
 
 	/**
-	 * Displays a message with the number of paths and length of the paths between 2
-	 * characters in the given text box.
+	 * Displays a message with the number of paths and degrees of separation between
+	 * 2 characters in the given text box.
 	 */
-	public void displayMessage(int numPaths, int pathLength, Text text, String char1, String char2);
+	public void displayMessage(int numPaths, int degrees, Text text, String char1, String char2);
 
 	/**
 	 * Displays the provided path in the give wrapper
