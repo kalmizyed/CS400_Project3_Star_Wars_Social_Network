b2343f96ea94f2cd81648b86415b68b80414417e
Kaden Almizyed <kalmizyed@wisc.edu>
2022-05-01 15:54:55 -0500

    Added GraphBuilderPlaceholderBD to keep unit tests isolated

---
+++ GraphBuilderPlaceholderBD.java
@@ -0,0 +1,22 @@
+// --== CS400 File Header Information ==--
+// Name: Kaden Almizyed
+// Email: kalmizyed@wisc.edu
+// Team: DC
+// TA: April
+// Lecturer: Florian
+// Notes to Grader:
+
+/**
+ * Placeholder GraphBuilder class for backend development.
+ */
+public class GraphBuilderPlaceholderBD implements IGraphBuilder {
+
+    /**
+     * Placeholder graph loader.  Returns an empty graph.
+     */
+    @Override
+    public ExtendedGraphADT<String> getGraph() {
+        return new ExtendedGraphPlaceholderBD();
+    }
+    
+}
